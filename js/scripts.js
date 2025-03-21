b10e2055e4d3d2c616a2c2500de85d7da273526f
Macky93 <ia.macar@ufg.edu.sv>
2025-03-21 10:58:57 -0600

    Update scripts.js
    js corregido

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,47 @@
-let mensaje =document.getElementById("mensajito")
-console.log(mensaje.innerText);
+let mensaje = document.getElementById("mensajito");
+console.log(mensaje.textContent);
 
-let variable; //declara una variable vacia
+let variable;
 
-variable = 1.666; //asigana un valor
+variable = 1.666;
+console.log("El tipo de dato es: " + typeof variable);
+console.log("El valor del dato es: " + variable);
 
-console.log("El tipo de dato es : "+typeof variable); //conocer tipo de dato de una variable
-console.log("El valor del dato es :"+variable); //conocer valor de variable
+variable = false;
+console.log("El tipo de dato es: " + typeof variable);
+console.log("El valor del dato es: " + variable);
 
-variable = false // asigna un valor a la variable 
-console.log("El tipo de dato es : "+typeof variable); //conocer tipo de dato de una variable
-console.log("El valor del dato es :"+variable); //conocer valor de variable
+variable = "Hola mundo";
+console.log("El tipo de dato es: " + typeof variable);
+console.log("El valor del dato es: " + variable);
 
-variable = "Hola mundo" // asigna un valor a la variable 
-console.log("El tipo de dato es : "+typeof variable); //conocer tipo de dato de una variable
-console.log("El valor del dato es :"+variable); //conocer valor de 
+function calcular(sumando1, sumando2, salida) {
+    let sumando1_obj = document.getElementById(sumando1);
+    let sumando2_obj = document.getElementById(sumando2);
 
-function calcular(sumando1,sumando2,salida){
-let sumando1_obj = document.getElementById("sumando1");
-let sumando2_obj = document.getElementById("sumando2");
+    let calculo = parseFloat(sumando1_obj.textContent) + parseFloat(sumando2_obj.textContent);
 
-let calculo = (parseFloat(sumando1_obj.innerText) + parseFloat(sumando2_obj.innerText));
-
-document.getElementById(salida).innerHTML = calculo;
-
+    document.getElementById(salida).textContent = calculo;
 }
 
-function crearElemento(url= null){
-    if (url!==null){
-        url == url
+function crearElemento(url = null) {
+    if (url === null) {
+        url = "https://as2.ftcdn.net/jpg/01/09/00/83/220_f_109008395_OQDupHMza1V6CNOzrJwWaklaktT4IsRW.jpg";
     }
-    else{
-        url = "https://as2.ftcdn.net/jpg/01/09/00/83/220_f_109008395_OQDupHMza1V6CNOzrJwWaklaktT4IsRW.jpg"
-    }
-    let objetoimagen = document.crearElemento("img");
+    
+    let objetoimagen = document.createElement("img");
     objetoimagen.id = "imagencreada";
     objetoimagen.src = url;
     objetoimagen.alt = "Icono sobre control de videojuego";
     objetoimagen.title = "Control de videojuego";
-    objetoimagen.width =220;
+    objetoimagen.width = 220;
     objetoimagen.height = 165;
-    objetoimagen.addEventListener('click', function(){
-        objetoimagen.src = "https://encrypted-tbn0.gstatic.com/image?q=tbn:ANd9GcTku5595AC-bbsoPF-Ik0WzUbVj9rnAbs91kw&s";
+
+    objetoimagen.addEventListener("click", function () {
+        objetoimagen.src = "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg";
         objetoimagen.width = 225;
         objetoimagen.height = 225;
     });
-    document.getElementById('lienzo').appendChild(objetoimagen);
+
+    document.getElementById("lienzo").appendChild(objetoimagen);
 }
